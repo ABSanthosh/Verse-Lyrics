7b259732e85ee49fcdf7f4fbe275ff045392d51b
ABSanthosh <a.b.santhosh02@gmail.com>
2021-05-23 00:17:47 +0530

    MuteAds Fragment added

---
+++ app/src/main/java/com/absan/verse/ui/MuteAds.kt
@@ -0,0 +1,19 @@
+package com.absan.verse.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.absan.verse.R
+
+class MuteAds : DialogFragment() {
+    override fun getTheme() = R.style.RoundedCornersDialog
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment__mute_ad, container, false)
+    }
+}
