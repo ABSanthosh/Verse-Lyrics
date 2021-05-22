b2c2fc29df74096370b6764eb43b2ac02a769f5a
ABSanthosh <a.b.santhosh02@gmail.com>
2021-05-23 00:18:58 +0530

    App rating, Feedback, Source code functions added

---
+++ app/src/main/java/com/absan/verse/Utils/RateOnPlayStore.kt
@@ -0,0 +1,24 @@
+package com.absan.verse.Utils
+
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.core.content.ContextCompat.startActivity
+
+fun RateOnPlayStore(context: Context) {
+    val uri = Uri.parse("market://details?id=com.absan.verse")
+    val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+
+    try {
+        startActivity(context, goToMarket, null)
+    } catch (e: ActivityNotFoundException) {
+        startActivity(
+            context,
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("http://play.google.com/store/apps/details?id=com.absan.verse")
+            ), null
+        )
+    }
+}
