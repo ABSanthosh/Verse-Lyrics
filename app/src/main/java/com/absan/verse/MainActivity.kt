8efea9b869a949a9c28558bbc6b150b5adf26ba2
ABSanthosh <a.b.santhosh02@gmail.com>
2021-05-25 01:52:30 +0530

    Navbar and navbar related fragments added and modified

--- app/src/main/java/com/absan/verse/MainActivity.kt
+++ app/src/main/java/com/absan/verse/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.absan.verse
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -21,7 +23,15 @@ var count = 0
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    lateinit var toggle: ActionBarDrawerToggle;
+    private lateinit var toggle: ActionBarDrawerToggle
+    private val loggerServiceIntentForeground by lazy {
+        Intent(
+            "START_FOREGROUND",
+            Uri.EMPTY,
+            this,
+            Logger::class.java
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Set full screen
@@ -35,7 +45,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             )
         }
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity__main)
 
         // Navigation drawer -- Start
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
@@ -51,14 +61,36 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
         toggle.isDrawerIndicatorEnabled = true
 
-        val NavigationView = findViewById<NavigationView>(R.id.navView)
-        NavigationView.setNavigationItemSelectedListener(this)
-        NavigationView.bringToFront()
+        val navigationView = findViewById<NavigationView>(R.id.navView)
+        navigationView.setNavigationItemSelectedListener(this)
+        navigationView.bringToFront()
 
         val logo: TextView = findViewById(R.id.logo)
         logo.visibility = View.VISIBLE
         //Navigation Drawer -- End
 
+        // Toggle for Ad mute function - Start
+        val menuItem: MenuItem = navigationView.menu.findItem(R.id.adblockmenu)
+        val toggleButton: androidx.appcompat.widget.SwitchCompat =
+            menuItem.actionView.findViewById(R.id.MuteAds__toggle)
+        toggleButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                this.startService(loggerServiceIntentForeground)
+                Toast.makeText(
+                    this,
+                    "Muting service started",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                this.stopService(loggerServiceIntentForeground)
+                Toast.makeText(
+                    this,
+                    "Muting service ended",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        // Toggle for Ad mute function - End
     }
 
     override fun onBackPressed() {
@@ -88,6 +120,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
             R.id.aboutapp -> {
                 About().show(supportFragmentManager, "About")
+            }
+            R.id.font -> {
+                FontSelector().show(supportFragmentManager, "Font")
             }
             R.id.mode -> {
             }
