5d81463142ff514fa3e456946849ff4182c11bba
ABSanthosh <a.b.santhosh02@gmail.com>
2021-05-29 04:10:04 +0530

    Font query builder from google's example app

---
+++ app/src/main/java/com/absan/verse/Utils/FontQueryBuilder.kt
@@ -0,0 +1,23 @@
+package com.absan.verse.Utils
+
+internal class QueryBuilder(
+    private val familyName: String,
+    val width: Float? = null,
+    private val weight: Int? = null,
+    private val italic: Float? = null,
+    private val besteffort: Boolean? = null
+) {
+
+    fun build(): String {
+        if (weight == null && width == null && italic == null && besteffort == null) {
+            return familyName
+        }
+        val builder = StringBuilder()
+        builder.append("name=").append(familyName)
+        weight?.let { builder.append("&weight=").append(weight) }
+        width?.let { builder.append("&width=").append(width) }
+        italic?.let { builder.append("&italic=").append(italic) }
+        besteffort?.let { builder.append("&besteffort=").append(besteffort) }
+        return builder.toString()
+    }
+}
