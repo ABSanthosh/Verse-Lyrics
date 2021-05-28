f1263e4c36a8f357c80cd1a25b8fe5158f5ee83f
ABSanthosh <a.b.santhosh02@gmail.com>
2021-05-29 04:07:22 +0530

    Fully implemented font selector including all google fonts

---
+++ app/src/main/java/com/absan/verse/Utils/FontRequester.kt
@@ -0,0 +1,52 @@
+package com.absan.verse.Utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.os.Handler
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.provider.FontRequest
+import androidx.core.provider.FontsContractCompat
+import com.absan.verse.R
+import com.absan.verse.data.Constants.TYPEFACE
+
+fun requestCustomFont(
+    context: Context,
+    familyName: String,
+    mHandler: Handler,
+    textView: TextView? = null,
+    setConstant:Boolean = true
+) {
+    val queryBuilder = QueryBuilder(familyName)
+    val mainPrefInstance by lazy { context.getSharedPreferences("main", Context.MODE_PRIVATE) }
+    val query = queryBuilder.build()
+
+//    Log.d("Fontname", "Requesting a font. Query: $query")
+    val request = FontRequest(
+        "com.google.android.gms.fonts",
+        "com.google.android.gms",
+        query,
+        R.array.com_google_android_gms_fonts_certs
+    )
+
+
+    val callback = object : FontsContractCompat.FontRequestCallback() {
+        override fun onTypefaceRetrieved(typeface: Typeface) {
+            mainPrefInstance.edit().apply { putString("FontQuery", query) }.apply()
+            if(setConstant) TYPEFACE = typeface
+            if(textView!=null) textView.typeface = typeface
+        }
+
+        override fun onTypefaceRequestFailed(reason: Int) {
+            Toast.makeText(context, "Unable to fetch font", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    if (familyName != "null" || familyName != null) {
+        FontsContractCompat
+            .requestFont(context, request, callback, mHandler)
+    }
+
+}
---
+++ app/src/main/java/com/absan/verse/Utils/resetLyricsView.kt
@@ -0,0 +1,23 @@
+package com.absan.verse.Utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.Log
+import android.widget.TableLayout
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import com.absan.verse.R
+import com.absan.verse.data.Constants.TYPEFACE
+
+
+fun ResetLyricView(table: TableLayout, context: Context? = null) {
+    for(i in 0 until table.childCount-1) {
+        if (table.getChildAt(i) != null) {
+            (table.getChildAt(i) as TextView).setTypeface(
+                if(TYPEFACE != Typeface.DEFAULT) TYPEFACE else (
+                    ResourcesCompat.getFont(context!!,R.font.walter_turncoat)),
+                Typeface.NORMAL
+            )
+        }
+    }
+}
