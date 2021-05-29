b63da7f46e18b3c00c47d6eba8f3303c90a4368f
ABSanthosh <a.b.santhosh02@gmail.com>
2021-05-29 19:19:57 +0530

    New theme changes and set theme class

---
+++ app/src/main/java/com/absan/verse/Utils/ThemeHelper.kt
@@ -0,0 +1,29 @@
+package com.absan.verse.Utils
+
+import androidx.appcompat.app.AppCompatDelegate
+
+object ThemeHelper {
+    const val LIGHT_MODE = "light"
+    const val DARK_MODE = "dark"
+    const val DEFAULT_MODE = "default"
+
+
+    fun applyTheme(themePref: String, isDarkThemeOn: Boolean = false) {
+        when (themePref) {
+            LIGHT_MODE -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            DARK_MODE -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            DEFAULT_MODE -> {
+                if (isDarkThemeOn) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+
+            }
+        }
+    }
+}
