00f7e7a06bcd47a282b6787e6035f323b3c775db
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-23 15:14:36 +0530

    Settings page

--- app/src/main/java/com/absan/verse/Settings.kt
+++ app/src/main/java/com/absan/verse/Settings.kt
@@ -1,5 +1,8 @@
 package com.absan.verse
 
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -7,8 +10,21 @@ import androidx.drawerlayout.widget.DrawerLayout
 
 class Settings : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.settings)
+        super.onCreate(savedInstanceState)
+
+        findViewById<ImageButton>(R.id.settingBackButton).setOnClickListener {
+            finish()
+        }
+    }
+
     override fun onBackPressed() {
-        finish();
+        try {
+            finish();
+        } catch (err: Exception) {
+            super.onBackPressed()
+        }
     }
 
 }
