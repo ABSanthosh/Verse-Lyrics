68aec41d1fa2bb876ce06299fc5a790607350ec4
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-31 02:25:13 +0530

    temp working font selector UI

--- app/src/main/java/com/absan/verse/Settings.kt
+++ app/src/main/java/com/absan/verse/Settings.kt
@@ -1,25 +1,39 @@
 package com.absan.verse
 
+import android.content.Context
+import android.graphics.Typeface
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.content.res.ResourcesCompat
 import com.absan.verse.Utils.*
+import com.absan.verse.data.Constants
 import com.aigestudio.wheelpicker.WheelPicker
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import java.util.*
+import kotlin.concurrent.schedule
 
 
 class Settings : AppCompatActivity() {
     private var RickRollcount = 0
+    private lateinit var mHandler: Handler
+    lateinit var textContent: TextView
+    private val mainPrefInstance by lazy { getSharedPreferences("main", Context.MODE_PRIVATE) }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.settings)
         super.onCreate(savedInstanceState)
+
+        val handlerThread = HandlerThread("fonts")
+        handlerThread.start()
+        mHandler = Handler(handlerThread.looper)
 
         findViewById<LinearLayout>(R.id.setting__textSize).setOnClickListener {
 
@@ -53,12 +67,34 @@ class Settings : AppCompatActivity() {
 
             val fontList = resources.getStringArray(R.array.family_names).toMutableList()
 
+            textContent = fontSelectorModal.findViewById(R.id.setting__fontSelector__sample)!!
+
             fontRoller?.data = fontList
 
-//            Log.e(
-//                "Roller",
-//                fontList.get
-//            )
+            Timer("SettingUp", false).schedule(300) {
+                fontRoller?.selectedItemPosition =
+                    fontList.indexOf(mainPrefInstance.getString("FontQuery", null))
+
+            }
+            requestCustomFont(
+                context = this,
+                familyName = mainPrefInstance.getString("FontQuery", null).toString(),
+                mHandler = mHandler,
+                textView = textContent,
+                setConstant = false
+            )
+
+            fontSelectorModal.findViewById<Button>(R.id.setDefault)?.setOnClickListener {
+                mainPrefInstance.edit().apply {
+                    putString("FontQuery", null)
+                }.apply()
+                Constants.TYPEFACE = ResourcesCompat.getFont(this, R.font.walter_turncoat)!!
+                textContent.typeface = ResourcesCompat.getFont(this, R.font.walter_turncoat)
+            }
+
+            fontSelectorModal.findViewById<Button>(R.id.setFont)?.setOnClickListener {
+                Constants.TYPEFACE = textContent.typeface
+            }
 
 
             fontRoller?.setOnItemSelectedListener { fontRoller, fontList, position ->
@@ -66,11 +102,16 @@ class Settings : AppCompatActivity() {
 
                     val fontListData =
                         resources.getStringArray(R.array.family_names).toMutableList()
-                    Log.e("Roller", fontListData.get(position))
 
+                    requestCustomFont(
+                        context = this,
+                        familyName = fontListData.get(position),
+                        mHandler = mHandler,
+                        textView = textContent,
+                        setConstant = false
+                    )
                 }
             }
-
 
             fontSelectorModal.show()
         }
