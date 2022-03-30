624f2e3313f8f0a41946d6020eb2731fd4df639f
ABSanthosh <a.b.santhosh02@gmail.com>
2022-03-30 23:36:10 +0530

    fetches font name and put it in wheel

--- app/src/main/java/com/absan/verse/Settings.kt
+++ app/src/main/java/com/absan/verse/Settings.kt
@@ -2,6 +2,7 @@ package com.absan.verse
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.absan.verse.Utils.*
+import com.aigestudio.wheelpicker.WheelPicker
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 
@@ -40,6 +42,37 @@ class Settings : AppCompatActivity() {
             }
 
             textSizeModal.show()
+        }
+
+        findViewById<LinearLayout>(R.id.setting__textFont).setOnClickListener {
+            val fontSelectorModal = BottomSheetDialog(this)
+            fontSelectorModal.setContentView(R.layout.settings__fontselector)
+
+            val fontRoller =
+                fontSelectorModal.findViewById<WheelPicker>(R.id.setting__fontSelector__roller)
+
+            val fontList = resources.getStringArray(R.array.family_names).toMutableList()
+
+            fontRoller?.data = fontList
+
+//            Log.e(
+//                "Roller",
+//                fontList.get
+//            )
+
+
+            fontRoller?.setOnItemSelectedListener { fontRoller, fontList, position ->
+                run {
+
+                    val fontListData =
+                        resources.getStringArray(R.array.family_names).toMutableList()
+                    Log.e("Roller", fontListData.get(position))
+
+                }
+            }
+
+
+            fontSelectorModal.show()
         }
 
         findViewById<ImageButton>(R.id.settingBackButton).setOnClickListener {
