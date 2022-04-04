fd1b05d257ea59c2ab7f77b4f4de2f3035c06bc4
ABSanthosh <a.b.santhosh02@gmail.com>
2022-04-04 18:01:36 +0530

    optimized imports

--- app/src/main/java/com/absan/verse/Utils/resetLyricsView.kt
+++ app/src/main/java/com/absan/verse/Utils/resetLyricsView.kt
@@ -2,7 +2,6 @@ package com.absan.verse.Utils
 
 import android.content.Context
 import android.graphics.Typeface
-import android.util.Log
 import android.widget.TableLayout
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
@@ -13,11 +12,11 @@ import com.absan.verse.data.Constants.TYPEFACE
 
 fun ResetLyricView(table: TableLayout, context: Context? = null) {
 //    TODO: Add -1 to length to avoid licence if its there
-    for(i in 0 until table.childCount) {
+    for (i in 0 until table.childCount) {
         if (table.getChildAt(i) != null) {
             (table.getChildAt(i) as TextView).setTypeface(
-                if(TYPEFACE != Typeface.DEFAULT || context == null) TYPEFACE else (
-                    ResourcesCompat.getFont(context,R.font.walter_turncoat)),
+                if (TYPEFACE != Typeface.DEFAULT || context == null) TYPEFACE else (
+                        ResourcesCompat.getFont(context, R.font.walter_turncoat)),
                 Typeface.NORMAL
             )
             (table.getChildAt(i) as TextView).textSize = FONTSIZE
