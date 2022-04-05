c7d074d57ace7ff2d834dd100173494ebcbae131
ABSanthosh <a.b.santhosh02@gmail.com>
2022-04-05 18:35:20 +0530

    Fixed unnecessary conditions

--- app/src/main/java/com/absan/verse/Utils/resetLyricsView.kt
+++ app/src/main/java/com/absan/verse/Utils/resetLyricsView.kt
@@ -10,16 +10,15 @@ import com.absan.verse.data.Constants.FONTSIZE
 import com.absan.verse.data.Constants.TYPEFACE
 
 
-fun ResetLyricView(table: TableLayout, context: Context? = null) {
+fun ResetLyricView(table: TableLayout, context: Context, stuff: String = "def") {
 //    TODO: Add -1 to length to avoid licence if its there
     for (i in 0 until table.childCount) {
+        val lyricsLine = (table.getChildAt(i) as TextView)
         if (table.getChildAt(i) != null) {
-            (table.getChildAt(i) as TextView).setTypeface(
-                if (TYPEFACE != Typeface.DEFAULT || context == null) TYPEFACE else (
-                        ResourcesCompat.getFont(context, R.font.walter_turncoat)),
-                Typeface.NORMAL
-            )
-            (table.getChildAt(i) as TextView).textSize = FONTSIZE
+            lyricsLine.typeface =
+                if (TYPEFACE != Typeface.DEFAULT) TYPEFACE else (
+                        ResourcesCompat.getFont(context, R.font.walter_turncoat))
+            lyricsLine.textSize = FONTSIZE
         }
     }
 }
