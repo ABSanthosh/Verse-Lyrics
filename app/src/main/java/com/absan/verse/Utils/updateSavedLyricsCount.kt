9ff1e36d54f76343d2cb4dd64d57a61638ddaa12
ABSanthosh <a.b.santhosh02@gmail.com>
2022-04-26 16:17:49 +0530

    Update current saved lyrics count

---
+++ app/src/main/java/com/absan/verse/Utils/updateSavedLyricsCount.kt
@@ -0,0 +1,14 @@
+package com.absan.verse.Utils
+
+import android.content.Context
+import android.widget.TextView
+import androidx.drawerlayout.widget.DrawerLayout
+import com.absan.verse.R
+import com.absan.verse.Utils.DatabaseRelated.DatabaseHandler
+
+fun updateSavedLyricsCount(context:Context, drawerLayout:DrawerLayout){
+    if(DatabaseHandler(context).readLyrics().size == 0)
+        drawerLayout.findViewById<TextView>(R.id.navbar__savedLyricsCount).text = "-"
+    else
+        drawerLayout.findViewById<TextView>(R.id.navbar__savedLyricsCount).text = DatabaseHandler(context).readLyrics().size.toString()
+}
