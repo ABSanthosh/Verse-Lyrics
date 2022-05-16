f37741835e48f848c20131c045dc0909db09768d
ABSanthosh <a.b.santhosh02@gmail.com>
2022-05-16 15:21:41 +0530

    type checks

--- app/src/main/java/com/absan/verse/data/RecentlyPlayed__ItemAdapter.kt
+++ app/src/main/java/com/absan/verse/data/RecentlyPlayed__ItemAdapter.kt
@@ -11,6 +11,7 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.absan.verse.R
+import com.absan.verse.Utils.openSpotify
 
 class RecentlyPlayed__ItemAdapter(
     val context: Context,
@@ -18,9 +19,9 @@ class RecentlyPlayed__ItemAdapter(
 ) : RecyclerView.Adapter<RecentlyPlayed__ItemAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val songName = view.findViewById<TextView>(R.id.recentlyPlayed__songName)
-        val ArtistName = view.findViewById<TextView>(R.id.recentlyPlayed__artistName)
-        val SongID = view.findViewById<TextView>(R.id.recentlyPlayed__songId)
+        val songName: TextView = view.findViewById(R.id.recentlyPlayed__songName)
+        val artistName: TextView = view.findViewById(R.id.recentlyPlayed__artistName)
+        val songID: TextView = view.findViewById(R.id.recentlyPlayed__songId)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,8 +29,9 @@ class RecentlyPlayed__ItemAdapter(
             LayoutInflater.from(context).inflate(R.layout.recentlyplayed__child, parent, false)
 
         val songIdTextview = view.findViewById<TextView>(R.id.recentlyPlayed__songId)
+
         view.findViewById<ImageView>(R.id.recentlyPlayed__openSpotify).setOnClickListener {
-            OpenSongInSpotify(songIdTextview.text.toString())
+            openSpotify(context, songIdTextview.text.toString())
         }
 
         return ViewHolder(view)
@@ -37,15 +39,10 @@ class RecentlyPlayed__ItemAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = songList[position]
-        holder.SongID.text = item.id
+        holder.songID.text = item.id
         holder.songName.text = item.track
-        holder.ArtistName.text = item.artist
+        holder.artistName.text = item.artist
     }
 
     override fun getItemCount() = songList.size
-
-    fun OpenSongInSpotify(id: String) {
-        val launcher = Intent(Intent.ACTION_VIEW, Uri.parse(id))
-        ContextCompat.startActivity(context, launcher, null)
-    }
 }
