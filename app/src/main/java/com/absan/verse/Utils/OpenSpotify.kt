aaa14cc282161e5e0c85e9cf5022b619e4980239
ABSanthosh <a.b.santhosh02@gmail.com>
2022-05-16 15:21:03 +0530

    option to open song if song id is given

--- app/src/main/java/com/absan/verse/Utils/OpenSpotify.kt
+++ app/src/main/java/com/absan/verse/Utils/OpenSpotify.kt
@@ -4,16 +4,22 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
 import kotlin.system.exitProcess
 
-fun OpenSpotify(context: Context) {
+fun openSpotify(context: Context, songId: String = "") {
     try {
-        val sendIntent = context.packageManager.getLaunchIntentForPackage("com.spotify.music")
-        if (sendIntent != null) {
-            startActivity(context, sendIntent, null)
+        if(songId == "") {
+            val sendIntent = context.packageManager.getLaunchIntentForPackage("com.spotify.music")
+            if (sendIntent != null) {
+                startActivity(context, sendIntent, null)
+            }
+        }else{
+            val launcher = Intent(Intent.ACTION_VIEW, Uri.parse(songId))
+            startActivity(context, launcher, null)
         }
-//        finishAffinity(sendIntent)
+
     } catch (e: java.lang.Exception) {
         startActivity(
             context,
