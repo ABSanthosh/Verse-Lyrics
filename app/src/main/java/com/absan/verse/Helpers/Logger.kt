678c788c6be0b79c248a03633190abf03787252a
ABSanthosh <a.b.santhosh02@gmail.com>
2022-05-20 15:34:09 +0530

    Moved to new Dir

--- app/src/main/java/com/absan/verse/Helpers/Logger.kt
+++ app/src/main/java/com/absan/verse/Helpers/Logger.kt
@@ -1,11 +1,10 @@
-package com.absan.verse.Utils
+package com.absan.verse.Helpers
 
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.media.AudioManager
 import android.os.IBinder
-import android.util.Log
 import com.absan.verse.data.Song
 import com.absan.verse.data.Spotify
 import com.absan.verse.data.isDuplicateOf
