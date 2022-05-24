b2e988b7a9824fb41e701972bda9cbdcd7ff304c
ABSanthosh <a.b.santhosh02@gmail.com>
2022-05-24 15:34:17 +0530

    Moved to new Dir

--- app/src/main/java/com/absan/verse/SavedSongs.kt
+++ app/src/main/java/com/absan/verse/SavedSongs.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.absan.verse.Utils.DatabaseRelated.BookmarkDatabaseHandler
+import com.absan.verse.Helpers.DatabaseRelated.BookmarkDatabaseHandler
 import com.absan.verse.data.SaveLyric__ItemAdapter
 
 class SavedSongs : AppCompatActivity() {
